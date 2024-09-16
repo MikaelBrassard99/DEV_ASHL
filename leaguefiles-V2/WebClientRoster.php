b8914727fc4ade99ee39fc20c0d15668ec8dab94
Mikael Brassard <beavers026@gmail.com>
2024-09-16 19:50:19 -0400

    update price ticket worked

--- leaguefiles-V2/WebClientRoster.php
+++ leaguefiles-V2/WebClientRoster.php
@@ -25,27 +25,20 @@
 		$LeagueOutputOption = $db->querySingle($Query,true);
 		
 		// Look for a team ID in the URL, if non exists use 0
-		if ($LeagueOutputOption['ShowWebClientInDymanicWebsite'] == "True"){
-			$t = (isset($_REQUEST["TeamID"])) ? filter_var($_REQUEST["TeamID"], FILTER_SANITIZE_NUMBER_INT): 0;
-			$row = array();
-			if($t > 0 AND $t <= 100){
-				$rs = api_dbresult_teamsbyname($db,"Pro",$t);
-				$row = $rs->fetchArray();
-			}
-		}
+		if ($LeagueOutputOption['ShowWebClientInDymanicWebsite'] == "True"){$t = (isset($_REQUEST["TeamID"])) ? filter_var($_REQUEST["TeamID"], FILTER_SANITIZE_NUMBER_INT): 0;}
 		// Make a default header 
 		// 5 Paramaters. PageID, database, teamid, League = Pro/Farm, $headcode (custom headercode can be added. DEFAULT "")
 		api_layout_header("rostereditor",$db,$t,false,$WebClientHeadCode);
 		include "Menu.php";
-		$DoNotRequiredLoginDynamicWebsite = TRUE;
+		
 		if ($CookieTeamNumber == 102){$DoNotRequiredLoginDynamicWebsite = TRUE;} // Commish is allow to upload anything so we are using the code from the 'Do Not Required Login Dynamic Website' to achieve this goal.
-
+		
 		if ($LeagueOutputOption['ShowWebClientInDymanicWebsite'] == "False"){
 			echo "<div class=\"STHSDivInformationMessage\">" . $ThisPageNotAvailable . "<br /><br /></div>";
 		}elseif(($CookieTeamNumber == $t OR $DoNotRequiredLoginDynamicWebsite == TRUE) AND $t > 0 AND $t <= 100){
 			// Display the roster editor page using API.
 			// use 3 paramaters Database, TeamID, showH1Tag (DEFAULT true/false)   
-			if($t > 0){api_pageinfo_editor_roster($db,$t);}
+			if($t > 0 AND $t <= 100){api_pageinfo_editor_roster($db,$t);}
 		}else{
 			echo "<div class=\"STHSDivInformationMessage\">" . $NoUserLogin . "<br /><br /></div>";
 		}
